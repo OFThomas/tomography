fc08be2dee460f91f2135b8a7cdae2e7b7cbe723
John Scott <johnrscott0@gmail.com>
2018-06-07 23:53:42 +0100

    Added functions for user interaction

--- input.py
+++ input.py
@@ -28,5 +28,21 @@ def yes_or_no(string):
             response = input("\n" + string + " [y/n]")
 
 # Get an integer from the user
-def get_user_int(string):
-    return int(input(string + " "))
+#   NEEDS TESTING!!
+#   Argument 1: A message to print for the benefit of the user
+#   Argument 2: The type of the requested value
+def get_user_value(string, type):
+    while 1==1:
+        response = input(string + " " + "[" + type + "]:")
+        try:
+            if type == "integer" : user_val = int(response)
+            elif type == "float" : user_val = float(response)
+            elif type == "complex" : user_val = complex(response)
+            return user_val
+        except ValueError:
+            print(response,"is not of type","[" + type + "]:","; try again...")
+
+# Get float from the user
+# NEEDS TESTING!!
+def get_user_float(string):
+    return float(input(string + " "))
