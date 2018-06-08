1202941fa4c5ade8a9eee5594eff75081ae54cab
johnrscott <johnrscott0@gmail.com>
2018-06-08 16:18:33 +0100

    Updated the simulation functions

--- simulation.py
+++ simulation.py
@@ -84,37 +84,39 @@ def density(dp):
             else: exit()
         return dens
 
-def simulate(dens,meas,dp):
-    yes_or_no("Is it correct")
-    print("The measurements are X, Y and Z:\n\n",meas[0],"\n\n",meas[1],"\n\n and \n\n",meas[2],".\n")
-    print("X is",meas[0])
-    eigenvalues =  np.zeros(3,dtype=complex).tolist()
-    eigenvectors = np.zeros(3,dtype=complex).tolist()
+def simulate(dens,meas_ops,dp):
+    print("\n===== Generate measurement data =====\n")
+    print("The X measurements are simulated by")
+    print("computing the projectors P = |+><+| and ")
+    print("P = |-><-| for each measurement outcome")
+    print("and then computing the probability using")
+    print("prob = tr(pP), where p is the density")
+    print("matrix. Then samples are taken from")
+    print("a discrete distribution defined by the")
+    print("measurement probabilities. The process")
+    print("is repeated for Y and Z.\n")
+    eigenvalues = {} #np.zeros(3,dtype=complex).tolist()
+    eigenvectors = {} #np.zeros(3,dtype=complex).tolist()
     # Compute the eigenvectors and eigenvalues of X, Y and Z
-    for n in range(3):
-        eigenvalues[n], eigenvectors[n] = np.linalg.eig(np.asmatrix(meas[n]))
-        # The eigenvectors should be real
-        print("The eigenvectors of X are\n\n", eigenvectors[n],"\n")
-        print("And the eigenvalues of X are:", eigenvalues[n])
-        # Python stores the eigenvectors as the columns of a matrix, so
-        # a corresponding eigenvalue-eigenvector pair is accessed
-        # like X_values[n], X_vectors[:,n] where n is 0 or 1.
+    for key in meas_ops:
+        eigenvalues[key], eigenvectors[key] = np.linalg.eig(np.asmatrix(meas_ops[key]))
 
     # Compute the projectors for X, Y and Z:
-    proj = np.zeros((3,2),dtype=complex).tolist()
-    p = proj
-    for n in range(3):
-        for m in range(2):
-            proj[n][m] = eigenvectors[n][:,m] * np.matrix.getH(eigenvectors[n][:,m])    
-            p[n][m] = np.trace(dens * proj[n][m]).real
-            print("The probability of",n,m," are:", np.around(p[n][m],dp))
-
+    proj = {}#np.zeros((3,2),dtype=complex).tolist()
+    p = {}#proj
+    for key in meas_ops:
+        proj[key] = ([eigenvectors[key][:,0] * np.matrix.getH(eigenvectors[key][:,0]),
+                      eigenvectors[key][:,1] * np.matrix.getH(eigenvectors[key][:,1])])
+        p[key] = ([np.trace(dens * proj[key][0]).real, np.trace(dens * proj[key][1]).real])
+    print("Finished calculating the projectors.\n")
+    for key in meas_ops:
+        print("The probabilities associated with",key,"are:", np.around(p[key],dp))
+        
     # Generate the measurement data
-    prob =  np.zeros(3,dtype=complex).tolist()
-    meas = prob
+    meas_dat = {}
     # User input: select number of measurements
-    samples = get_user_int("Choose the number of measurements in each basis:")
-    for n in range(3):
-        meas[n] = np.random.choice(eigenvalues[n], samples, p=p[n]);
+    samples = get_user_value("Choose the number of measurements in each basis:","integer")
+    for key in meas_ops:
+        meas_dat[key] = np.random.choice(eigenvalues[key], samples, p=p[key]);
 
-    return meas[0], meas[1], meas[2]
+    return meas_dat
