3c455f27fb7b04b01f92eeceabe0f0930af50a4c
johnrscott <johnrscott0@gmail.com>
2018-06-26 16:52:59 +0100

    Fixed a bug

--- stats.py
+++ stats.py
@@ -65,6 +65,23 @@ def distance_trace(A,B):
     
 def distance_fid(A,B):
     values_A,vectors_A = np.linalg.eig(A)
+    B_new = np.matmul(np.matmul(np.linalg.inv(vectors_A), B), vectors_A)
+    A_new = np.diag(np.sqrt(values_A))
+    D = np.asmatrix(A_new) * np.asmatrix(B_new) * np.asmatrix(A_new)
+    values_D = np.linalg.eigvals(D)
+    #fidelity = np.sum(np.linalg.eigvals(D))
+    fidelity = np.sum(np.sqrt(values_D))
+
+    C = sc.linalg.sqrtm(A) 
+    fidelity1 = np.matrix.trace(sc.linalg.sqrtm(C * B * C))
+    print(fidelity1-fidelity)
+    exit(1)
+    distance = np.arccos(fidelity).real
+    return distance
+
+# Use this one if you know the eigenvectors and eigenvalues of
+# A already
+def distance_fid_2(vectors_A,values_A,B):
     B_new = np.linalg.inv(np.matmul(np.matmul(vectors_A, B), vectors_A))
     A_new = np.diag(np.sqrt(values_A))
     D = np.asmatrix(A_new) * np.asmatrix(B_new) * np.asmatrix(A_new)
