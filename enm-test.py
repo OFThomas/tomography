a42c83ac46e48e3f52975f6140b6554418b47450
johnrscott <johnrscott0@gmail.com>
2018-07-12 09:57:15 +0100

    Updated estimation

---
+++ enm-test.py
@@ -0,0 +1,229 @@
+#!/usr/local/bin/python3
+#############################################################
+# Title: Testing extended norm minimisation
+#
+# Date created: 11th June 2018
+#
+# Language:    Python 3
+#
+# Overview:    The program tests the extended norm
+#              minimisation algorithm in the case
+#              of an informationally complete set of
+#              measurements 
+#
+# Details:     The script performs the following steps:
+#
+#              1) Set x in [0,1] as the purity parameter;
+#                 pick a random unitary U; and compute
+#
+#
+#                          -       -
+#                         | x     0 |
+#                 p = U * |         | * U^
+#                         | 0   1-x |
+#                          -       -
+#
+#                 The closer x is to 1 or 0, the more
+#                 pure is the state.
+#
+#              2) Fix a set of measurement operators,
+#                 and generate sample data from the
+#                 distribution of outcomes implied by
+#                 the state p.
+#
+#              3) Compute the density matrix using the
+#                 extended norm minimsation.
+#
+#              4) Compute the distance between the
+#                 estimate and the true density matrix
+#                 for each of the different distances
+#                 (operator norm, Hilbert-Schmidt norm,
+#                 fidelity).
+#
+#              5) Perform averages of each distance at each
+#                 value of x. Plot the average distances
+#                 as a function of x for all the values of
+#                 x.
+#
+#              6) Repeat steps 1-4 for different values
+#                 of x and repeat each value of x a large
+#                 number of times. Store all the distances.
+#
+# Usage: python3 enm-test.py
+#
+#############################################################
+
+# Include
+import importlib
+import numpy as np
+from scipy.stats import unitary_group as ug
+import scipy as sc
+import simulation
+importlib.reload(simulation)
+import estimation
+importlib.reload(estimation)
+import stats
+importlib.reload(stats)
+import matplotlib.pyplot as plt
+import estimation
+importlib.reload(estimation)
+from matplotlib.offsetbox import AnchoredText
+import cProfile
+import pstats
+
+pr = cProfile.Profile()
+pr.enable()
+
+# ======= Test parameter ===============================
+M = 200  # Number of purity parameters x to try
+x_start = 0 # Specify purity parameter x range
+x_end = 1
+N = 100  # Number of random density matrices per x value
+S = 100  # Number of samples of each measurement to
+        # simulate for each density matrix 
+# ======================================================
+
+av_distances = np.zeros([M,3])
+non_physical = np.zeros([M,1]) # Proportion of non-physical estimates
+
+# Preliminaries: compute the projectors
+
+I = np.matrix([[1,0],[0,1]])
+X = np.matrix([[0,1],[1,0]])
+Y = np.matrix([[0,-1j],[1j,0]])
+Z = np.matrix([[1,0],[0,-1]])
+
+values_X, vectors_X = np.linalg.eig(X)
+proj_X = np.zeros([2,2,2])
+proj_X[0,:,:] = np.matmul(vectors_X[:,0], np.matrix.getH(vectors_X[:,0]))
+proj_X[1,:,:] = np.matmul(vectors_X[:,1], np.matrix.getH(vectors_X[:,1]))
+
+values_Y, vectors_Y = np.linalg.eig(Y)
+proj_Y = np.zeros([2,2,2])
+proj_Y[0,:,:] = np.matmul(vectors_Y[:,0], np.matrix.getH(vectors_Y[:,0]))
+proj_Y[1,:,:] = np.matmul(vectors_Y[:,1], np.matrix.getH(vectors_Y[:,1]))
+
+values_Z, vectors_Z = np.linalg.eig(Z)
+proj_Z = np.zeros([2,2,2])
+proj_Z[0,:,:] = np.matmul(vectors_Z[:,0], np.matrix.getH(vectors_Z[:,0]))
+proj_Z[1,:,:] = np.matmul(vectors_Z[:,1], np.matrix.getH(vectors_Z[:,1]))
+
+# Define x -- put a loop here ----------------------------------------- LOOP for x between 0 and 1
+#
+# This loop runs through different values of the purity parameter x,
+# and tests the ability of the linear estimator in each case
+#
+
+dist_op = np.zeros([N,1])
+dist_trace = np.zeros([N,1])
+dist_fid = np.zeros([N,1])
+
+dp = 5
+
+#for k,n in itertools.product(range(M),range(N)):
+for k in range(M):
+    non_physical_count = 0 # Temporary counter for non-physical estimates
+    
+    # Loop N times for each value of x ------------------ inner loop -- N trials for each x
+    #
+    # This loop generates N random density matrices for each fixed value of x
+    # which used to simulate measurement data and run the estimator
+    #
+    for n in range(N):
+        # Step 1: Prepare the density matrix
+        #
+        # The purity parameter x is picked between 0
+        # and 1.
+        #
+        # Note: any time a numerical check is performed
+        # and printed out, I've rounded the result to
+        # make it more readable. Set the decimal places
+        # to keep using the dp variable.
+        #
+        x = x_start + k * (x_end - x_start)/M
+        #pr.enable()
+        dens = simulation.random_density(x)
+        #values_dens,vectors_dens = np.linalg.eig(dens)
+        #pr.disable()
+        # Step 2: Generate measurement data
+        #
+        # Generate data for X, Y and Z measurements
+        #
+        #pr.enable()
+        X_data = simulation.simulate(dens,proj_X,values_X,S)
+        Y_data = simulation.simulate(dens,proj_Y,values_Y,S)
+        Z_data = simulation.simulate(dens,proj_Z,values_Z,S)
+
+        proj = np.concatenate((proj_X, proj_Y, proj_Z), axis=0)
+        #print(proj)
+
+        #print(result)
+        #pr.disable()
+        # Step 3: Estimate density matrix
+        #
+        # Compute linear estimator
+        #
+        # Then tr(pI) is computed by requiring that
+        # the density matrix be normalised
+        #
+        dens_est = estimation.ENM.enm_XYZ(X_data, Y_data, Z_data)[0]
+        
+        # Step 4: Compute and the distances
+        #
+        # Compute distances between the estimated
+        # and true density matrix using the
+        # different distance fuctions.
+        #
+        dist_op[n] = stats.distance_op(dens, dens_est)
+        dist_trace[n] = stats.distance_trace(dens, dens_est)
+        #pr.enable()
+        dist_fid[n] = stats.distance_fid(dens, dens_est)
+        #dist_fid[n] = stats.distance_fid_2(values_dens,vectors_dens,dens_est)
+        #pr.disable()
+        # Count the number of non-physical matrices
+        #
+        eigenvalues = np.linalg.eigvals(dens_est)
+        if eigenvalues[0] < 0 or eigenvalues[1] < 0:
+            non_physical_count = non_physical_count + 1
+        
+    # Step 5: Average the distances 
+    #
+    # Average the distances for each value of x
+    #
+    print(eigenvalues[0])
+    av_distances[k,:] = [np.mean(dist_op), np.mean(dist_trace), np.mean(dist_fid)]
+    non_physical[k] = non_physical_count/N
+
+pr.disable()
+pr.create_stats()
+
+ps = pstats.Stats(pr)
+total_time = ps.total_tt
+pr.print_stats(sort='cumulative')
+
+# Step 6: Process the data 
+#
+# Store in a file, plot, etc.
+x_values = np.linspace(x_start, x_end, M)
+
+fig, ax1 = plt.subplots()
+ax1.set_xlabel('Purity parameter')
+ax1.set_ylabel('Estimate error distance')
+ax1.plot(x_values, av_distances[:,0], '.',color='tab:red', label='Operator distance')
+ax1.plot(x_values, av_distances[:,1], '.',color='tab:green', label='Trace distance' )
+ax1.plot(x_values, av_distances[:,2], '.',color='tab:blue', label='Fidelity')
+ax1.legend(loc=3, ncol=1)
+
+ax2=ax1.twinx()
+ax2.set_ylabel('Probability')
+ax2.plot(x_values,non_physical, '+', color='tab:brown',label='Non-physical')
+atext = AnchoredText("Number of purity parameters: " + str(M) + "\n"
+                     +"Density matrices: "+ str(N) + "\n"
+                     +"Samples per measurement: " + str(S) + "\n"
+                     +"Total running time:  " + str(np.around(total_time,3)) + "s",
+                     loc=2)
+ax2.add_artist(atext)
+ax2.legend(loc=1)
+
+fig.tight_layout()
+plt.show()
--- estimation.py
+++ estimation.py
@@ -55,112 +55,123 @@ def linear_estimate_XYZ(X_data, Y_data, Z_data):
     dens_est = (mean_X * X + mean_Y * Y + mean_Z * Z + I)/2
     return dens_est
 
-# Function: maximum_likelihood_XYZ(X_data, Y_data, Z_data)
-#
-# This function estimates the density matrix using
-# X, Y and Z data using the maximum likelihood method
-#
-# The likelihood of the density matrix rho given
-# N measurements outcomes P_n is
-#
-#              _N_
-#             |   |
-#    L[rho] = |   | Tr[rho * P_n]  
-#             n = 0
-#
-# There are six projectors for X, Y and Z;
-# one for each outcome +1 or -1. If X, Y and
-# Z are each measured S times, then N = 3S and
-# there are N terms in the product. The first
-# S terms correspond to X, the second to Y and
-# the last to Z. 
-#
-# The proj array contains the X, Y and Z projectors
-# in the order X, Y and Z, with + before -. 
-#
-# The parameter space is specified as follows:
-# Let rho = T^ T where T is a lower triangular
-# complex matrix with real elements on the
-# diagonal. This guarantees that rho is
-# Hermitian and positive. Four parameters are
-# necessary to specify T in the one qubit case.
-#
-# The trace = 1 condition is fulfilled using
-# the method of lagrange multipliers. If
-#
-#          _       _
-#         | a     0 |
-#     T = |         |
-#         | b+jc  d |
-#          ^       ^
-#
-def maximum_likelihood_XYZ(X_data, Y_data, Z_data, proj):
+
+class ML:
+
+    # Function: maximum_likelihood_XYZ(X_data, Y_data, Z_data)
+    #
+    # This function estimates the density matrix using
+    # X, Y and Z data using the maximum likelihood method
+    #
+    # The likelihood of the density matrix rho given
+    # N measurements outcomes P_n is
+    #
+    #              _N_
+    #             |   |
+    #    L[rho] = |   | Tr[rho * P_n]  
+    #             n = 0
+    #
+    # There are six projectors for X, Y and Z;
+    # one for each outcome +1 or -1. If X, Y and
+    # Z are each measured S times, then N = 3S and
+    # there are N terms in the product. The first
+    # S terms correspond to X, the second to Y and
+    # the last to Z. 
+    #
+    # The proj array contains the X, Y and Z projectors
+    # in the order X, Y and Z, with + before -. 
+    #
+    # The parameter space is specified as follows:
+    # Let rho = T^ T where T is a lower triangular
+    # complex matrix with real elements on the
+    # diagonal. This guarantees that rho is
+    # Hermitian and positive. Four parameters are
+    # necessary to specify T in the one qubit case.
+    #
+    # The trace = 1 condition is fulfilled using
+    # the method of lagrange multipliers. If
+    #
+    #          _       _
+    #         | a     0 |
+    #     T = |         |
+    #         | b+jc  d |
+    #          ^       ^
+    #
+    @staticmethod
+    def XYZ(X_data, Y_data, Z_data, proj):
     
-    def L(x,args):
-        # x specifies rho = T T^
+        def L(x,args):
+            # x specifies rho = T T^
+            rho = np.matmul(np.array([[x[0], 0],[x[1]+1j*x[2], x[3]]]),np.array([[x[0], x[1]-1j*x[2]],[0, x[3]]]))
+            tmp = 1
+            M = args[:,0,0].size
+            for n in range(0,M):
+                tmp = tmp * np.real(np.trace(np.matmul(rho, args[n,:,:])))
+            tmp = tmp - x[4] * np.real(np.trace(rho)) # Lagrange multiplier
+            return -tmp
+
+        def L_1(x,args):
+            # x specifies rho = T T^
+            rho = np.matmul(np.array([[x[0], 0],[x[1]+1j*x[2], x[3]]]),np.array([[x[0], x[1]-1j*x[2]],[0, x[3]]]))
+            print(rho)
+            tmp = 0
+            M = args[:,0,0].size
+            print(args)
+            exit(1)
+            for n in range(0,M):
+                tmp = tmp + np.log(np.real(np.trace(np.matmul(rho, args[n,:,:]))))
+                #print(np.trace(np.matmul(rho, args[n,:,:])))
+                #exit(1)
+            tmp = tmp - x[4] * np.real(np.trace(rho)) # Lagrange multiplier
+            print("tmp:",-tmp)
+            return -tmp
+
+        def L_2(x,args):
+            # x specifies rho = T T^
+            rho = np.matmul(np.array([[x[0], 0],[x[1]+1j*x[2], x[3]]]),np.array([[x[0], x[1]-1j*x[2]],[0, x[3]]]))
+            M = args[:,0,0].size
+            trace = np.zeros(M)
+            for n in range(0,M):
+                trace[n] = np.log(np.real(np.trace(np.matmul(rho, args[n,:,:]))))
+            total = -trace.sum() + x[4] * np.real(np.trace(rho)) # Lagrange multiplier
+            print("tot:",-total)
+            return total
+
+
+        data = np.concatenate((X_data, Y_data, Z_data), axis=0)
+        #print("X",X_data)
+        #print("Y",Y_data)
+        #print("Z",Z_data)
+        S = X_data.size
+        N = 3*S
+        args = np.zeros([N,2,2],dtype='complex')
+        for n in range(S):
+            if X_data[n] == +1 : args[n,:,:] = proj[0]
+            else : args[n,:,:] = proj[1]
+            if Y_data[n] == +1 : args[n+S,:,:] = proj[2]
+            else : args[n+S,:,:] = proj[3]
+            if Z_data[n] == +1 : args[n+2*S,:,:] = proj[4]
+            else : args[n+2*S,:,:] = proj[5]
+
+        #print(args)
+        # fun1 = lambda var : var[0]**2 + var[1]**2 + var[2]**2 + var[3]**2 - 1
+        # cons = ({'type':'eq', 'fun':fun1},
+        #         {'type':'eq', 'fun':lambda x : (x[0]**2)*(x[3]**2) - 0.25},
+        #         {'type':'ineq', 'fun':lambda x : x[0]},
+        #         {'type':'ineq', 'fun':lambda x : x[3]})
+
+        # cons1 = ({'type':'ineq', 'fun':lambda x : x[0]},
+        #          {'type':'ineq', 'fun':lambda x : x[3]})
+
+        #result = sc.optimize.minimize(L_2, [1,0,0,0,0], args=args, constraints=cons1, method='TNC')
+        #result = sc.optimize.minimize(L_2, [1,0,0,0,0], args=args)
+        x = sc.optimize.fmin(L_2, [1,0,0,0,0], args=(args,))
+        exit(1)
+        #print(result)
+        #exit(1)
+        #x = result.xopt
         rho = np.matmul(np.array([[x[0], 0],[x[1]+1j*x[2], x[3]]]),np.array([[x[0], x[1]-1j*x[2]],[0, x[3]]]))
-        tmp = 1
-        M = args[:,0,0].size
-        for n in range(0,M):
-            tmp = tmp * np.real(np.trace(np.matmul(rho, args[n,:,:])))
-        tmp = tmp - x[4] * np.real(np.trace(rho)) # Lagrange multiplier
-        return -tmp
-
-    def L_1(x,args):
-        # x specifies rho = T T^
-        rho = np.matmul(np.array([[x[0], 0],[x[1]+1j*x[2], x[3]]]),np.array([[x[0], x[1]-1j*x[2]],[0, x[3]]]))
-        print("rho:",rho)
-        tmp = 0
-        M = args[:,0,0].size
-        for n in range(0,M):
-            tmp = tmp + np.log(np.real(np.trace(np.matmul(rho, args[n,:,:]))))
-            #print(np.trace(np.matmul(rho, args[n,:,:])))
-            #exit(1)
-        tmp = tmp - x[4] * np.real(np.trace(rho)) # Lagrange multiplier
-        print("tmp:",tmp)
-        return -tmp
-
-    def L_2(x,args):
-        # x specifies rho = T T^
-        rho = np.matmul(np.array([[x[0], 0],[x[1]+1j*x[2], x[3]]]),np.array([[x[0], x[1]-1j*x[2]],[0, x[3]]]))
-        M = args[:,0,0].size
-        trace = np.zeros(M)
-        for n in range(0,M):
-            trace[n] = np.log(np.real(np.trace(np.matmul(rho, args[n,:,:]))))
-        total = -trace.sum() + x[4] * np.real(np.trace(rho)) # Lagrange multiplier
-        return total
-
-    
-    data = np.concatenate((X_data, Y_data, Z_data), axis=0)
-    S = X_data.size
-    N = 3*S
-    args = np.zeros([N,2,2],dtype='complex')
-    for n in range(S):
-        if X_data[n] == +1 : args[n,:,:] = proj[0]
-        else : args[n,:,:] = proj[1]
-        if Y_data[n] == +1 : args[n+S,:,:] = proj[2]
-        else : args[n+S,:,:] = proj[3]
-        if Z_data[n] == +1 : args[n+2*S,:,:] = proj[4]
-        else : args[n+2*S,:,:] = proj[5]
-
-    #print(args)
-
-    fun1 = lambda var : var[0]**2 + var[1]**2 + var[2]**2 + var[3]**2 - 1
-    cons = ({'type':'eq', 'fun':fun1},
-            {'type':'eq', 'fun':lambda x : (x[0]**2)*(x[3]**2) - 0.25},
-            {'type':'ineq', 'fun':lambda x : x[0]},
-            {'type':'ineq', 'fun':lambda x : x[3]})
-
-    cons1 = ({'type':'ineq', 'fun':lambda x : x[0]},
-             {'type':'ineq', 'fun':lambda x : x[3]})
-    
-    result = sc.optimize.minimize(L_2, [1,0,0,0,0], args=args, constraints=cons1, method='TNC')
-    #result = sc.optimize.fmin(L_1, [1,0,0,0,0], args=args)
-    #print(result)
-    #exit(1)
-    x = result.x
-    rho = np.matmul(np.array([[x[0], 0],[x[1]+1j*x[2], x[3]]]),np.array([[x[0], x[1]-1j*x[2]],[0, x[3]]]))
-    return rho
+        return rho
 
 
 
@@ -171,16 +182,19 @@ def maximum_likelihood_XYZ(X_data, Y_data, Z_data, proj):
 #
 # 
 #
-def enm_XYZ(X_data, Y_data, Z_data):
+class ENM():
 
-    def d(x,rho_2):
-        # x specifies rho = T T^
-        rho_1 = np.matmul(np.array([[x[0], 0],[x[1]+1j*x[2], x[3]]]),np.array([[x[0], x[1]-1j*x[2]],[0, x[3]]]))
-        distance = sts.distance_op(rho_1, rho_2)
-        return distance    
+    @staticmethod
+    def enm_XYZ(X_data, Y_data, Z_data):
 
-    rho_linear = linear_estimate_XYZ(X_data, Y_data, Z_data)
-    result = sc.optimize.minimize(d, [1,1,1,1], args=rho_linear)
-    x = result.x
-    rho = np.matmul(np.array([[x[0], 0],[x[1]+1j*x[2], x[3]]]),np.array([[x[0], x[1]-1j*x[2]],[0, x[3]]]))
-    return rho, rho_linear
+        def d(x,rho_2):
+            # x specifies rho = T T^
+            rho_1 = np.matmul(np.array([[x[0], 0],[x[1]+1j*x[2], x[3]]]),np.array([[x[0], x[1]-1j*x[2]],[0, x[3]]]))
+            distance = sts.distance_op(rho_1, rho_2)
+            return distance    
+
+        rho_linear = linear_estimate_XYZ(X_data, Y_data, Z_data)
+        result = sc.optimize.minimize(d, [1,1,1,1], args=rho_linear)
+        x = result.x
+        rho = np.matmul(np.array([[x[0], 0],[x[1]+1j*x[2], x[3]]]),np.array([[x[0], x[1]-1j*x[2]],[0, x[3]]]))
+        return rho, rho_linear
