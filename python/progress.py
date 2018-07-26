dbf1cb85d39a4d3d6f9cae1286ee07586f3325c5
Oliver Thomas <oliver.thomas@bristol.ac.uk>
2018-07-26 14:41:58 +0100

    remove numpy dependency

--- python/progress.py
+++ python/progress.py
@@ -2,7 +2,6 @@
 
 import cProfile
 import pstats
-import numpy as np
 
 def show_progress(pr,p):
     rem = "?"
@@ -11,9 +10,9 @@ def show_progress(pr,p):
     ps = pstats.Stats(pr)
     pr.enable()
     t = ps.total_tt
-    if p != 0 : rem = str(np.around(t/p - t,3))
-    print("\u001b[0m\u001b[1mPercent done:\u001b[32m",str(np.around(100*p,2))+"%\u001b[0m\u001b[1m",
-          "\tTotal time:\u001b[34m",str(np.around(t,2))+"s\u001b[0m\u001b[1m", 
+    if p != 0 : rem = str(round(t/p - t,3))
+    print("\u001b[0m\u001b[1mPercent done:\u001b[32m",str(round(100*p,2))+"%\u001b[0m\u001b[1m",
+          "\tTotal time:\u001b[34m",str(round(t,2))+"s\u001b[0m\u001b[1m", 
           "\tEstimated time remaining:\u001b[35m",str(rem)+"s\u001b[0m\u001b[1m",
           end='                 \r')
     
